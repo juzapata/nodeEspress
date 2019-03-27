493455e5e8a5cb48e98752de2d1687054a4310dd
Julia Zapata <juliamzapata@gmail.com>
2019-03-27 20:28:46 -0300

    using redirectiong - not working

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,7 +4,9 @@ var express = require('express');
 var router = express.Router();
 
 let landing = require("../controllers/landing");
+console.log("landing");
 /* GET home page. */
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 // o primeiro index é a variável criada acima, e o segundo é o metodo que tem dentro do inde.js que esta na pasta controllers
 module.exports = router;
